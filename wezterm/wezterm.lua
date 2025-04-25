b522a791de557e98668494be04419f729821f5a8
Jimmie Fulton <jimmie.fulton@gmail.com>
2025-04-25 11:59:04 -0700

    fonts: change weight and size for Maple Mono NF

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -23,9 +23,9 @@ config.colors = {
 }
 
 -- Fonts
-config.font_size = 13.0
+config.font_size = 14.0
 -- config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = 500 })
-config.font = wezterm.font("Maple Mono NF", { weight = 500 })
+config.font = wezterm.font("Maple Mono NF", { weight = 400 })
 
 -- Behavior
 config.send_composed_key_when_left_alt_is_pressed = false
